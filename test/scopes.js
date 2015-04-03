e8a14c595650b3219d0f89352d90966c0d4f250c
Amit Portnoy <amit.portnoy@gmail.com>
2015-04-03 10:47:06 +0300

    continue adding scope (will replace reveal paths)

--- test/scopes.js
+++ test/scopes.js
@@ -17,7 +17,7 @@ describe('scopes', function () {
     assert.ok(Scope.MASTER.includes(null, {$scope: Scope.MASTER}));
   });
 
-  it('default snapshot returns everything', function () {
+  it('default and MASTER snapshots return everything', function () {
     var o = Master.newInstance({
       n: {$type: Number, $scope: Scope.PUBLIC},
       s: {$type: String, $scope: 'customScope'},
@@ -26,34 +26,16 @@ describe('scopes', function () {
       a: {$type: Master.FixedArray, $elem: {n: {$type: Number, $scope: Scope.MASTER}}, $length: 3}
     });
 
+    var expected = {
+      n: 0,
+      s: '',
+      b: false,
+      o: {n: 0},
+      a: [{n: 0}, {n: 0}, {n: 0}]
+    };
 
-    var s = o.$snapshot();
-    assert.property(s, 'n');
-    assert.property(s, 's');
-    assert.property(s, 'b');
-    assert.property(s, 'o');
-    assert.property(s.o, 'n');
-    assert.property(s, 'a');
-    assert.property(s.a[0], 'n');
-  });
-
-  it('snapshot with MASTER scope returns everything', function () {
-    var o = Master.newInstance({
-      n: {$type: Number, $scope: Scope.PUBLIC},
-      s: {$type: String, $scope: 'customScope'},
-      b: {$type: Boolean},
-      o: {$type: {n: {$type: Number, $scope: Scope.MASTER}}, $scope: Scope.MASTER},
-      a: {$type: Master.FixedArray, $elem: {n: {$type: Number, $scope: Scope.MASTER}}, $length: 3}
-    });
-
-    var s = o.$snapshot(Scope.MASTER);
-    assert.property(s, 'n');
-    assert.property(s, 's');
-    assert.property(s, 'b');
-    assert.property(s, 'o');
-    assert.property(s.o, 'n');
-    assert.property(s, 'a');
-    assert.property(s.a[0], 'n');
+    assert.deepEqual(o.$snapshot(), expected);
+    assert.deepEqual(o.$snapshot(Scope.MASTER), expected);
   });
 
   it('snapshot with public scope return only properties with default or public scope', function () {
@@ -65,30 +47,89 @@ describe('scopes', function () {
       a: {$type: Master.FixedArray, $elem: {n: {$type: Number, $scope: Scope.MASTER}}, $length: 3}
     }).$snapshot(Scope.PUBLIC);
 
-    assert.property(s, 'n');
-    assert.notProperty(s, 's');
-    assert.property(s, 'b');
-    assert.property(s, 'o');
-    assert.notProperty(s.o, 'n');
-    assert.property(s, 'a');
-    assert.notProperty(s.a, 'n');
+    assert.deepEqual(s, {
+      n: 0,
+      b: false,
+      o: {},
+      a: [{}, {}, {}]
+    });
   });
 
-  //it('snapshot with custom scope can control what they return', function () {
-  //  var spy = sinon.spy(function(_, config) {return config.hasOwnProperty('$scope') && config.$scope === 'customScope'});
-  //
-  //  var s = Master.newInstance({
-  //    n: {$type: Number, $scope: Scope.PUBLIC},
-  //    s: {$type: String, $scope: 'customScope'},
-  //    b: {$type: Boolean},
-  //    o: {$type: {n: {$type: Number, $scope: Scope.MASTER}}},
-  //    a: {$type: Master.FixedArray, $elem: {n: {$type: Number, $scope: Scope.MASTER}}, $length: 3}
-  //  }).$snapshot({includes: spy});
-  //
-  //  assert(spy.calledCount);
-  //  //assert.property(o, 'n');
-  //  //assert.notProperty(o, 's');
-  //  //assert.property(o, 'b');
-  //  //assert.notProperty(o, 'o');
-  //});
+  it('snapshot with custom scope can control what they return', function () {
+    var spy = sinon.spy(function(_, config) {
+      return config.hasOwnProperty('$scope') && config.$scope === 'customScope'
+    });
+
+    var s = Master.newInstance({
+      n: {$type: Number, $scope: Scope.PUBLIC},
+      s: {$type: String, $scope: 'customScope'},
+      b: {$type: Boolean},
+      o: {$type: {n: {$type: Number, $scope: Scope.MASTER}}},
+      a: {$type: Master.FixedArray, $elem: {n: {$type: Number, $scope: Scope.MASTER}}, $length: 3}
+    }).$snapshot({includes: spy});
+
+    assert.strictEqual(spy.callCount, 5); // (it doesn't enter the array and object sub elements)
+    assert(spy.calledWith(['s'], {$getter: false, $primitive: true, $scope: 'customScope', $type: String}));
+
+    assert.deepEqual(s, {
+      s: '',
+    });
+  });
+
+  // TODO move this a new npm module
+  it('supports a player scope use case', function () {
+    var PLAYER_SCOPE = function(playerIdx) {
+      if (this === undefined) {
+        return new PLAYER_SCOPE(playerIdx);
+      }
+      this.playerIdx = playerIdx;
+    };
+    PLAYER_SCOPE.prototype.includes = function (keyPath, config) {
+      if (!config.hasOwnProperty('$scope')) {
+        return true;
+      }
+
+      if (config.$scope === PLAYER_SCOPE) {
+        if (keyPath[0] !== 'players') {
+          // TODO consider how to fail early when creating the master object instead of in player snapshot time
+          throw Error('the PLAYER scope should only be nested inside a top level players array');
+        } else {
+          return (keyPath[1] === this.playerIdx);
+        }
+      } else {
+        return config.$scope === Scope.PUBLIC;
+      }
+    };
+
+    var s = Master.newInstance({
+      n: {$type: Number, $scope: Scope.PUBLIC},
+      b: {$type: Boolean},
+      o: {$type: {n: {$type: Number, $scope: Scope.MASTER}}},
+      players: {
+        $type: Master.FixedArray,
+        $length: 3,
+        $elem: {
+          playerSecret: {
+            $type: Number,
+            $initialValue: 42,
+            $scope: PLAYER_SCOPE
+          },
+          playerPub: {
+            $type: Number
+          }
+        }
+      }
+    }).$snapshot(PLAYER_SCOPE(1));
+
+    assert.deepEqual(s, {
+      n: 0,
+      b: false,
+      o: {},
+      players: [
+        {playerPub: 0},
+        {playerPub: 0, playerSecret: 42},
+        {playerPub: 0}
+      ]
+    });
+  });
 });
